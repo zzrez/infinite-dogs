bdceed6e1a81bf9373cdc5c6c14006f7feec9224
zzrez <paul@speedrez.com>
2019-10-16 08:45:49 -0600

    Remove config

--- config.js
+++
@@ -1,6 +0,0 @@
-//UnShare API key (should NOT appear in github)
-const config = {
-    accessKey: "6a45f873313fae6b31dd16bb8fc1b39c6500297655b7ae19d627775ce25ce9ac",
-  }
-  
-  export default config
